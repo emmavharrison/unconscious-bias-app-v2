fb9f506b9d7fb3db9d3b91e6a0d6cd24e9b53051
Nicola Hearn <nfhearn@gmail.com>
2024-03-28 14:41:17 +0000

    we have a landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,28 @@
+import Robot from "../assets/robot.png";
+
 export function LandingPage() {
   return (
-    <>
-      <h1>HELLO</h1>
-    </>
+    <div className="w-9/12 h-30 bg-cyan-300 rounded-3xl p-5 center">
+      <h1 className="text-4xl mt-8">Welcome to our Ally Odyssey</h1>
+      <div className="flex items-center mr-10 mt-20">
+        <img src={Robot} className="float-right m-4 w-80" />
+        <p className="text-lg text-wrap ">
+          Hello and welcome to OpenMind Odyssey. Do you have a cultural bias
+          you're unaware of? Research shows 90% of us do and the statistics
+          suggest there is still a ton of work to do in order to value each
+          other equally.
+          <br />
+          <br />
+          We want to live in a world where we are not afraid to get things
+          wrong, can acknowledge our mistakes without shame and work to unlearn
+          our damaging behaviours. Learning to unlearn should be fun so lighten
+          up, don't be scared and be ready with an open mind. Are you racist,
+          ableist, sexist without realising?
+        </p>
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 text-xl rounded-lg focus:outline-none focus:shadow-outline mt-20">
+        Get my Ally Odyssey!
+      </button>
+    </div>
   );
 }
