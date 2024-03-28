c77d9fef03b65803695be62fe7b6ea5fd55ae72d
Emma Harrison <emma.harrison@dunelm.com>
2024-03-28 15:34:15 +0000

    add different results pages

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+export const getScore = () => {};
+
 export const Quiz = () => {
   const [currentQ, setCurrentQ] = useState(0);
   const [score, setScore] = useState([]);
@@ -42,8 +44,20 @@ export const Quiz = () => {
     setCurrentQ((old) => old - 1);
   };
 
-  const total = score.reduce((acc, curr) => acc + curr, 0);
-  console.log("total score", total);
+  const totalScore = score.reduce((acc, curr) => acc + curr, 0);
+
+  let toValue;
+
+  if (totalScore >= 0 && totalScore <= 4) {
+    toValue = "/resultsBad";
+  } else if (totalScore >= 5 && totalScore <= 10) {
+    toValue = "/resultsNeutral";
+  } else if (totalScore >= 11 && totalScore <= 16) {
+    toValue = "/resultsGood";
+  } else {
+    // Handle out of range values, if needed
+    toValue = "/results"; // Default to "/results" if totalScore is out of expected range
+  }
 
   return (
     <div className="grid grid-cols-[10%_80%_10%] items-center gap-4">
@@ -53,7 +67,7 @@ export const Quiz = () => {
         <Button onClick={next}>Next</Button>
       ) : (
         <Button asChild>
-          <Link to="/results">Submit</Link>
+          <Link to={toValue}> Submit </Link>
         </Button>
       )}
     </div>
